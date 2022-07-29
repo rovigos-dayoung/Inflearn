a36f88366e01276f3cf993d95f7bd28348e4144a
rovigos-dayoung <dyk205@rovigos.com>
2022-07-29 16:12:04 +0900

    정적 파일 연결

--- app.js
+++ app.js
@@ -3,12 +3,37 @@
 var express = require('express');  // express == function
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {  // get(): Router / get()가 하는 일: Routing / Rout: finding ways
   res.send('Hello homepage');
 });
+
+app.get('/dynamic', function(req, res) {
+  var output = `
+  <!DOCTYPE html>
+  <html lang="ko">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    Hello, Dynamic!
+  </body>
+  </html>`;
+  res.send(output);
+});
+
+app.get('/route', function(req, res) {
+  res.send('Hello Router, <img src="/dog.jpg">');
+});
+
 app.get('/login', function(req, res) {
   res.send('<h1>Login please</h1>');
 });
+
 app.listen(3000, function() {
   console.log('Connected 3000 port!');
 });
